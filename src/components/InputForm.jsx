d2be57be531f65e40f2aa580e72d84bb9a0231b7
Y.S <c1320527@st.kanazawa-it.ac.jp>
2026-02-06 11:21:45 +0900

    Update Alert

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -4,31 +4,43 @@ import { useTodo } from '../hooks/useTodo'
 const InputForm = () => {
     const { addTodo, isEditing } = useTodo();
     const [inputText, setInputText] = useState('');
+    const [error, setError] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (isEditing) {
-            alert('There is a task being edited.')
+            setError('There is a task being edited.')
             return
         };
 
         if (inputText.trim() === '') {
-            alert('Please enter the task details.');
+            setError('Please enter the task details.');
             return;
         }
 
         addTodo(inputText);
         setInputText('');
+        setError('');
     }
 
     return (
         <div className="inputForm">
             <form onSubmit={handleSubmit}>
-                <input type="text" onChange={e => setInputText(e.target.value)} value={inputText}/>
+                <input type="text"
+                        onChange={(e) => {
+                            setInputText(e.target.value)
+                            setError('');
+                        }}
+                        value={inputText}
+                        placeholder="Enter task details"/>
                 <button>
                     <i className="fa-solid fa-square-plus"></i>
                 </button>
             </form>
+
+            {error && (
+                <p className="error-message">{error}</p>
+            )}
         </div>
     )
 }
